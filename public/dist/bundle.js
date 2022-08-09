d6737d07bc5a24d2cc136de35da3318f99dee788
Ezequiel <ezevici123@gmail.com>
2022-08-09 18:37:51 -0300

    if MenuMobile

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -419,7 +419,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar MenuMostrar = document.querySelector('.MenuMostrar');\nvar menuMobile = document.querySelector('.MenuNavegacion');\nMenuMostrar.addEventListener('click', function () {\n  menuMobile.classList.toggle('Visible');\n});\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menuMobile);\n\n//# sourceURL=webpack://hecemitododulce/./public/js/Modulos/MenuMobile.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar MenuMostrar = document.querySelector('.MenuMostrar');\nvar menuMobile = document.querySelector('.MenuNavegacion');\n\nif (MenuMostrar) {\n  MenuMostrar.addEventListener('click', function () {\n    menuMobile.classList.toggle('Visible');\n  });\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menuMobile);\n\n//# sourceURL=webpack://hecemitododulce/./public/js/Modulos/MenuMobile.js?");
 
 /***/ }),
 
--- public/js/Modulos/MenuMobile.js
+++ public/js/Modulos/MenuMobile.js
@@ -1,9 +1,12 @@
 const MenuMostrar = document.querySelector('.MenuMostrar');
 const menuMobile = document.querySelector('.MenuNavegacion');
 
+
+if(MenuMostrar){
 MenuMostrar.addEventListener('click',function(){
     menuMobile.classList.toggle('Visible');
 })
+}
 
 
 export default menuMobile;
