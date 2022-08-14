6414f184a77b6e3ac7112e512a441c4be2e609f3
Ezequiel <ezevici123@gmail.com>
2022-08-14 16:22:45 -0300

    Actualizar refres email

--- config/email.js
+++ config/email.js
@@ -2,5 +2,5 @@ module.exports = {
     CLIENT_ID: '667053186947-f82bet5qhhig5huh5vtb5pp5qe27bovc.apps.googleusercontent.com',
     CLIENT_SECRET: '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',
     REDIRECT_URL: 'https://developers.google.com/oauthplayground',
-    REFRESH_TOKEN: '1//04XqTi4VQetThCgYIARAAGAQSNwF-L9IrEKRk7hBmIRBs3Ni7ROmP2IQ7EwPS-0Wwj9S5Cc88qSStKSRSooHwLQ6L_DZnDSKOSOc',
+    REFRESH_TOKEN: '1//04rBgp2kIuVWtCgYIARAAGAQSNwF-L9Ir51NOW9bqgKWN-on0afvPnzhfQkwUBER0KhmVLo4nIYGw7d8R_0Duv-dBP0RKohf5aLI',
 };
